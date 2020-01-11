2fad51a09e2890fcfd6428b83f888e57676c8a71
김유진 <kimyujin@gim-yujin-ui-MacBook-Pro.local>
2020-01-11 11:05:09 +0900

    first commit

---
+++ yuja.js
@@ -0,0 +1,18 @@
+const express_box = require('express')
+const app = express_box()
+
+app.get('/', function (req, res) {
+    res.send("idiot!")
+})
+
+app.get('/ya', function (req, res) {
+    res.send("응 도라가~")
+})
+
+// app.get('/', (req, res) => {
+//     res.send("hello world!")
+// })
+
+app.listen(666, function () {
+    console.log("System is working")
+})
